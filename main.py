cb1de74fcaa4582dd444897c14fad9a5de1662bb
EliaBessmeltseva <elia.mail333@gmail.com>
2019-10-16 00:26:35 +0700

    commit

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 # Case 1
 import turtle
+def square(x, y, size, color, angle):
+    #TO DO: (Настя) функция рисов. квадрата
+    pass
+def triangle(x, y, size, color, angle):
+    #TO DO: (Лера) функция рисов. треугольника
+    pass
+def parallelogram(x, y, size, color, angle):
+    #TO DO: (Эля) функция рисов. параллелограмма
+    pass
+#(Настя) фигура дом, лебедь
+#(Лера) фигура бабочка, лодка
+#(Эля) фигура лиса, рыба
