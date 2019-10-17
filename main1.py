7c1d02863e2fd1f94b5cf05d8591d150efb14019
NastyaGrosheva <a.grosheva@list.ru>
2019-10-17 16:54:23 +0200

    commit

--- main1.py
+++ main1.py
@@ -1,21 +1,32 @@
 # Case 1
 import turtle
-def square(x, y, size, color, angle):
-    #TO DO: (Настя) функция рисов. квадрата
-    pass
+
+def square(x, y, size, color, color2, angle):
+    turtle.color(color)
+    turtle.fillcolor(color2)
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.right(angle) # повернуть курсор на 90 градусов вправо
+    turtle.pendown()
+    for i in range(4):
+        turtle.forward(size) # пройти вперед на насстояние 70, если курсор опущен, то будет нарисована линия
+        turtle.right(angle)
+    return()
+
 def triangle(x, y, size1, size2, color, color2, angle1, angle2):
-turtle.color(color)
-turtle.fillcolor(color2)
-turtle.penup()
-turtle.goto(x, y)
-turtle.right(angle1)
-turtle.pendown()
-turtle.forward(size1)
-turtle.right(angle2)
-turtle.forward(size1)
-turtle.right(angle1) # Повернуть курсор влево на 45 градусов
-turtle.forward(size2)
-return()
+    turtle.color(color)
+    turtle.fillcolor(color2)
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.right(angle1)
+    turtle.pendown()
+    turtle.forward(size1)
+    turtle.right(angle2)
+    turtle.forward(size1)
+    turtle.right(angle1) # Повернуть курсор влево на 45 градусов
+    turtle.forward(size2)
+    return()
+
 def parallelogram(x, y, size, color, angle):
     #TO DO: (Эля) функция рисов. параллелограмма
     pass
