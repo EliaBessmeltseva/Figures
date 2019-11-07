cacf47cb92cab8893a91e12d39aab49e6e125024
EliaBessmeltseva <elia.mail333@gmail.com>
2019-11-08 00:07:48 +0700

    fox, fish

--- main.py
+++ main.py
@@ -47,6 +47,22 @@ def parallelogram(x, y, size1, size2, color, angle1, angle2, to_angle):
         turtle.right(angle2)
     turtle.end_fill()
     return()
+#fish
+triangle(0, -70, 90, 128, 'orange', 135, 90, 225)
+triangle(90, 20, 90, 128, 'green', 135, 90, -45)
+triangle(-2, 85, 45, 65, 'yellow', 135, 90, 45)
+triangle(-47, -7, 45, 65, 'violet', 135, 90, 135)
+square(-2, 38, 44, 'cyan', 90, 0)
+parallelogram(-46, 38, 44, 60, 'blue', 135, 45, 45)
+triangle(-90, 35, 60, 93, 'red', 135, 90, 90)
+#fox
+triangle(40, -34, 90, 128, 'orange', 135, 90, 45)
+parallelogram(73, -155, 44, 60, 'blue', 135, 45, -90)
+triangle(-24, 32, 90, 128, 'red', 135, 90, 90)
+triangle(-26, -59, 65, 93, 'yellow', 135, 90, 270)
+square(-57, 66, 44, 'cyan', 90, 45)
+triangle(-90, 100, 45, 65, 'violet', 135, 90, 90)
+triangle(-24, 36, 45, 65, 'green', 135, 90, 270)
 #(Nastya) figure house, swan
 #(Lera) figure butterfly, boat
 #(Elia) figure fox, fish
