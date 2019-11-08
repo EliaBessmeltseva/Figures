894727d3f01cae1f964de2cae6e51e787839742f
Shakirov Dmitry <shakirov_dima@inbox.ru>
2019-11-08 13:31:48 +0700

    commit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # Case 1
 import turtle
 
-def square(x, y, size, color, angle, to_angle):
+
+def square(x, y, size, color, angle, to_angle):         # Nastya
     turtle.penup()
     turtle.goto(x, y)
     turtle.setheading(to_angle)
@@ -15,7 +16,9 @@ def square(x, y, size, color, angle, to_angle):
         turtle.right(angle)
     turtle.end_fill()
     return()
-def triangle(x, y, size1, size2, color, angle1, angle2, to_angle):
+
+
+def triangle(x, y, size1, size2, color, angle1, angle2, to_angle):          # Lera
     turtle.penup()
     turtle.goto(x, y)
     turtle.setheading(to_angle)
@@ -31,7 +34,9 @@ def triangle(x, y, size1, size2, color, angle1, angle2, to_angle):
     turtle.forward(size2)
     turtle.end_fill()
     return()
-def parallelogram(x, y, size1, size2, color, angle1, angle2, to_angle):
+
+
+def parallelogram(x, y, size1, size2, color, angle1, angle2, to_angle):         # Elia
     turtle.penup()
     turtle.goto(x, y)
     turtle.setheading(to_angle)
@@ -47,7 +52,14 @@ def parallelogram(x, y, size1, size2, color, angle1, angle2, to_angle):
         turtle.right(angle2)
     turtle.end_fill()
     return()
-#fish
+
+
+# (Nastya) figure house, swan
+# (Lera) figure butterfly, boat
+# (Elia) figure fox, fish
+
+
+# fish
 triangle(0, -70, 90, 128, 'orange', 135, 90, 225)
 triangle(90, 20, 90, 128, 'green', 135, 90, -45)
 triangle(-2, 85, 45, 65, 'yellow', 135, 90, 45)
@@ -55,7 +67,9 @@ triangle(-47, -7, 45, 65, 'violet', 135, 90, 135)
 square(-2, 38, 44, 'cyan', 90, 0)
 parallelogram(-46, 38, 44, 60, 'blue', 135, 45, 45)
 triangle(-90, 35, 60, 93, 'red', 135, 90, 90)
-#fox
+
+
+# fox
 triangle(40, -34, 90, 128, 'orange', 135, 90, 45)
 parallelogram(73, -155, 44, 60, 'blue', 135, 45, -90)
 triangle(-24, 32, 90, 128, 'red', 135, 90, 90)
@@ -63,10 +77,9 @@ triangle(-26, -59, 65, 93, 'yellow', 135, 90, 270)
 square(-57, 66, 44, 'cyan', 90, 45)
 triangle(-90, 100, 45, 65, 'violet', 135, 90, 90)
 triangle(-24, 36, 45, 65, 'green', 135, 90, 270)
-#(Nastya) figure house, swan
-#(Lera) figure butterfly, boat
-#(Elia) figure fox, fish
-#butterfly
+
+
+# butterfly
 triangle(-30,-30,45,70,"red",135,90,225)
 square(-30,15,45,"green",90,180)
 triangle(-76,60,45,70,"gold",135,90,135)
@@ -74,7 +87,9 @@ triangle(15,60,90,128,"blue",135,90,315)
 triangle(106,148,90,128,"brown",135,90,45)
 triangle(107,56,65,95,"cyan",135,90,0)
 parallelogram(59,10,40,60,"pink",135,45,45)
-#boat
+
+
+# boat
 square(-30,-30,45,"gold",90,180)
 triangle(-30,15,45,70,"pink",135,90,45)
 triangle(-27,17,65,95,"green",135,90,0)
@@ -82,7 +97,9 @@ triangle(15,-30,45,70,"blue",135,90,225)
 parallelogram(63,16,45,70,"orange",135,45,135)
 triangle(-63,16,90,128,"red",135,90,180)
 triangle(-60,142,90,128,"magenta",135,90,90)
-#house
+
+
+# house
 square(-43, 113, 35, 'cyan', 90, 0)
 parallelogram(-78, 75, 35, 60, 'pink', 135, 45, -225)
 triangle(30, 30, 45, 65, 'red', 135, 90, 0)
@@ -90,7 +107,9 @@ triangle(32, -34, 45, 65, 'yellow', 135, 90, 270)
 triangle(-100, -34, 90, 128, 'blue', 135, 90, 180)
 triangle(-80, 33, 90, 128, 'orange', 135, 90, 180)
 triangle(-100, -34, 64, 70, 'green', 135, 90, 225)
-#swan
+
+
+# swan
 square(-82, 43, 43, 'blue', 90, 45)
 parallelogram(-82, 45, 60, 45, 'purple', 135, 45, 225)
 triangle(-85, 106, 45, 65, 'red', 135, 90, 45)
@@ -98,6 +117,8 @@ triangle(-114, 13, 45, 65, 'cyan', 135, 90, 90)
 triangle(60, -10, 90, 128, 'orange', 135, 90, 0)
 triangle(20, -100, 90, 128, 'green', 135, 90, -45)
 triangle(-70, -100, 64, 70, 'yellow', 135, 90, 270)
-#code has been checked on the site https://metaschool.ru/pub/konkurs/python/turtle/index.php
-#https://server.179.ru/tasks/python/2017b1/pgm12.5_Turtle.html - Main teams
-#https://docs.python.org/2/library/turtle.html - turtle
+
+
+# code has been checked on the site https://metaschool.ru/pub/konkurs/python/turtle/index.php
+# https://server.179.ru/tasks/python/2017b1/pgm12.5_Turtle.html - Main teams
+# https://docs.python.org/2/library/turtle.html - turtle
